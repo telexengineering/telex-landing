4d63204f91c6978db9698a5978af706fde8d9ad1
Mohammad SF <mohammad7293@gmail.com>
2020-11-30 21:19:56 +0330

    removing blurs

--- scripts/top-lang-menu.js
+++ scripts/top-lang-menu.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $("#top-lang-menu").click(function(e){
-        $("#nav-lang-dropdown-list").fadeToggle();
+        $("#nav-lang-dropdown-list").fadeToggle(250);
         
         e.stopPropagation();
         $("body").not("#nav-lang-dropdown-list").click(function(){
-          $("#nav-lang-dropdown-list").fadeOut();
+          $("#nav-lang-dropdown-list").fadeOut(250);
         });
     });
 });
